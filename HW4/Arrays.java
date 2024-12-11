21a683e6a2995d56dfb07555654b8c5d04258526
topfloorjason <hoj4@go.stockton.edu>
2024-12-10 23:41:15 -0500

    hw4

--- HW4/Arrays.java
+++ HW4/Arrays.java
@@ -1,5 +1,7 @@
 package HW4;
 
+import java.util.Scanner;
+
 public class Arrays {
 
     // CONVERT THE SUMS METHOD FROM HW3 TO ADD EACH VALUE TO AN ARRAY AND THEN OUTPUT THE 
@@ -22,8 +24,29 @@ public class Arrays {
     // Total: 22
     // Array: {12,2,3,4,1,0}
     // Note: You can not use Arrays class to do this. You must use a regular array. No Arrays.stream
-    public static void Arraysums(){
+    public static void Arraysums() {
+        int[] numbers = new int[10];
+        int total = 0;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("I will add up the numbers you give me....");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Number: ");
+            numbers[i] = scanner.nextInt();
+            total += numbers[i];
+            System.out.println("The total so far is " + total + ".");
+        }
+
+        System.out.println("Total: " + total);
+        System.out.print("Array: {");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i < numbers.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println("}");
     }
 
     // Create a method that will brute force a password EX.
@@ -39,14 +62,27 @@ public class Arrays {
     // Hint 1: using a char array for the password is helpful here 
     // Hint 2: using the method .toCharArray is a way to convert the password parameter to a char array as well
     // From there creating for and while loops to compare the values is trivial
-
-    public static String bruteForce(String password){
+    public static String bruteForce(String password) {
         String answer = "";
         char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         '!', '@', '#', '$', '%', '^', '&', '*', '(', ')','<','>','/','?'};
-    
-        return answer; 
+
+        char[] passwordChars = password.toCharArray();
+        char[] guess = new char[passwordChars.length];
+
+        for (int i = 0; i < passwordChars.length; i++) {
+            for (char c : alphabet) {
+                guess[i] = c;
+                System.out.println(new String(guess));
+                if (guess[i] == passwordChars[i]) {
+                    break;
+                }
+            }
+        }
+
+        answer = new String(guess);
+        return answer;
     }
 
     // Create a method that will sort an Array of integers from smallest to greatest.
@@ -56,19 +92,37 @@ public class Arrays {
 
     // Hint 1: a temp Array is a helpful way to store variables for comparing 
     // Hint 2: 2 nested For loops should be all thats needed for the swapping logic
-    public static int[] sorter(int[] nums){
-        int [] sortedArray = new int[nums.length];
-        return sortedArray;
+    public static int[] sorter(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    // swap nums[j] and nums[j + 1]
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
     }
-
-
-
 
     public static void main(String[] args) {
         // Tester main method for your methods
         System.out.println("HW4!");
 
-    }
+        // Test Arraysums
+        Arraysums();
 
-    
+        // Test bruteForce
+        System.out.println("Brute force result: " + bruteForce("ZZZZ"));
+
+        // Test sorter
+        int[] nums = {9, 10, 2, 5, 3, 4, 7, 8};
+        int[] sortedNums = sorter(nums);
+        System.out.print("Sorted array: ");
+        for (int num : sortedNums) {
+            System.out.print(num + " ");
+        }
+    }
 }
